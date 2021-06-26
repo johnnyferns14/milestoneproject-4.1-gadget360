f9d87a93e1d660dcecf7697a3efcab3418a62c6a
johnnyferns14 <johnnyferns@gmail.com>
2021-06-26 21:17:16 +0000

    Checkout form properly saved as per PEP guidelines

--- checkout/forms.py
+++ checkout/forms.py
@@ -31,3 +31,4 @@ class OrferForm(forms.ModelForm):
                 placeholder = f'{placeholders[field]}'
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
